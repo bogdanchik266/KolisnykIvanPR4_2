71195da214efc96426222eb731bcd70165326157
Ivan Kolisnyk <113425342+bogdanchik266@users.noreply.github.com>
2024-11-01 10:30:27 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+const btnOpenModal = document.querySelector('#btnOpenModal');
+const modalBlock = document.querySelector('#modalBlock');
+const closeModal = 	document.querySelector('#closeModal');
+const questionTitle = document.querySelector('#question');
+const formAnswers = document.querySelector('#formAnswers');
+
+btnOpenModal.addEventListener('click', () => {
+console.log(modalBlock);
+modalBlock.classList.add('d-block'); 
+playTest();
+})
+
+closeModal.addEventListener('click', () => {
+	modalBlock.classList.remove('d-block');
+})
+
+const playTest = () => {
+	const renderQuestions = () => {
+		questionTitle.textContent = 'Якого кольору бургер ви хочете?';
+
+		formAnswers.innerHTML = `<div class="answers-item d-flex flex-column">
+                <input type="radio" id="answerItem1" name="answer" class="d-none">
+                <label for="answerItem1" class="d-flex flex-column justify-content-between">
+                  <img class="answerImg" src="./image/burger.png" alt="burger">
+                  <span>Стандарт</span>
+                </label>
+              </div>
+              <div class="answers-item d-flex justify-content-center">
+                <input type="radio" id="answerItem2" name="answer" class="d-none">
+                <label for="answerItem2" class="d-flex flex-column justify-content-between">
+                  <img class="answerImg" src="./image/burgerBlack.png" alt="burger">
+                  <span>Чорний</span>
+                </label>
+              </div>`
+	}
+	renderQuestions();
+}
+
+})
+
+
+
+
